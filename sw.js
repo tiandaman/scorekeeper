c2a2ff433ff886b9899892b5bf18f28286b083a8
tiandaman <52509578+tiandaman@users.noreply.github.com>
2026-08-29 22:12:05 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='scorekeeper-v1';
+const FILES=['./','./index.html','./app.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
